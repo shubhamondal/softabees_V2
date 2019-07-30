adb380c911a8a6e3b972f2df444d3ba8a03af830
Shubha Mondal <mondal.shubha@gmail.com>
2019-07-30 21:45:30 +0530

    first

---
+++ contactform/contactform.js
@@ -0,0 +1,140 @@
+jQuery(document).ready(function ($) {
+  "use strict";
+
+  //Contact
+  $('form.contactForm').submit(function () {
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    f.children('input').each(function () { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+          case 'checked':
+            if (!i.is(':checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function () { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    if (ferror) return false;
+    else var str = $(this).serialize();
+    var regData = {};
+    regData['name'] = $('#name').val();
+    regData['email'] = $('#email').val();
+    regData['collage'] = $('#collage').val();
+    regData['coursename'] = $('#coursename').val();
+    regData['mobileno'] = $('#mobileno').val();
+    regData['subject'] = $('#subject').val();
+    regData['message'] = $('#message').val();
+
+    var GetData = {
+
+    };
+    GetData['name'] = $('#name').val();
+    GetData['email'] = $('#email').val();
+    GetData['subject'] = $('#subject').val();
+    GetData['message'] = $('#message').val();
+
+    var action = $(this).attr('action');
+    var data;
+    if (!action) {
+      if (regData['collage'] !== undefined && regData['coursename']!== undefined){
+        action = '../contactform/register.php';
+        data = regData;
+      }
+      else{
+        action = '../contactform/contactform.php';
+        data = GetData;
+      }
+    }
+
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: data,
+      success: function (msg) {
+        if (msg == 'OK') {
+          $("#sendmessage").addClass("show");
+          $("#errormessage").removeClass("show");
+          $('.contactForm').find("input, textarea").val("");
+        } else {
+          $("#sendmessage").removeClass("show");
+          $("#errormessage").addClass("show");
+          $('#errormessage').html(msg);
+        }
+
+      }
+    });
+    return false;
+  });
+
+});
